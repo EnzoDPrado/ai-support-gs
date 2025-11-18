94c9b5109a3d17062da6f7dbe9abb8dfa70884ea
Enzo D Prado <enzodp1501@gmail.com>
2025-11-18 20:21:04 -0300

    feat: route to list courses

--- src/resource/CourseResource.java
+++ src/resource/CourseResource.java
@@ -3,12 +3,10 @@ package resource;
 import dao.CourseDao;
 import dao.UserDao;
 import dto.curso.CriarCursoInputDTO;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.*;
 import usecases.curso.CriarCursoUseCase;
+import usecases.curso.ListarCursosPorUsuarioUseCase;
 
 import java.sql.SQLException;
 
@@ -19,9 +17,11 @@ import java.sql.SQLException;
 public class CourseResource {
 
     private final CriarCursoUseCase criarCursoUseCase;
+    private final ListarCursosPorUsuarioUseCase listarCursosPorUsuarioUseCase;
 
     public CourseResource() throws SQLException {
         criarCursoUseCase = new CriarCursoUseCase(new CourseDao(), new UserDao());
+        listarCursosPorUsuarioUseCase = new ListarCursosPorUsuarioUseCase(new CourseDao(), new UserDao());
     }
 
     @POST
@@ -34,9 +34,13 @@ public class CourseResource {
         return Response.created(uri.build()).entity(course).build();
     }
 
-//    @PUT
-//    @Path("/{id}")
-//    public void atualizar(@PathParam("id") Long id, AtualizarUsuarioInputDTO input, @Context UriInfo uriInfo) throws SQLException {
-//        this.atualizarUsuarioUseCase.execute(input, id);
-//    }
+    @GET
+    @Path("/usuarios/{id}")
+    public Response listarPorUsuario(@PathParam("id") Long id, @Context UriInfo uriInfo) throws SQLException {
+        UriBuilder uri = uriInfo.getAbsolutePathBuilder();
+
+        final var courses = this.listarCursosPorUsuarioUseCase.execute(id);
+
+        return Response.ok(uri.build()).entity(courses).build();
+    }
 }
