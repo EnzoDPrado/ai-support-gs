cac3476eb7562429e5444a058f913194f49dc104
Enzo D Prado <enzodp1501@gmail.com>
2025-11-19 19:22:27 -0300

    feat: ocult password

--- src/infra/DatabaseConnection.java
+++ src/infra/DatabaseConnection.java
@@ -7,8 +7,8 @@ import java.util.Objects;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
-    private static final String USUARIO = "rm556905";
-    private static final String SENHA = "030105";
+    private static final String USUARIO = "your_user";
+    private static final String SENHA = "your_password";
     private static Connection connection = null;
 
 
